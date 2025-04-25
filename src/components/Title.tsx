a57ca4a97be798caf81b29d7fd109a53adb9c31e
Ryan Loftus <ryanloftus02@outlook.com>
2025-04-25 01:14:28 -0400

    adding scroll indicator to landing screen

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 export const Title: React.FC<{}> = ({}) => (
-  <div className="h-screen flex items-center justify-center">
-    <h1 className="text-6xl font-semibold mt-48 mb-16" id="title">Hey, I'm Ryan</h1>
+  <div className="relative h-screen w-screen flex items-center justify-center">
+    <div className="text-center">
+      <h1 className="text-6xl font-semibold" id="title">Hey, I'm Ryan</h1>
+    </div>
+
+    <div className="absolute text-center bottom-8 w-screen text-2xl animate-bounce opacity-70">
+      <span className="border-slate-600 border-2 rounded-full px-2 py-1">↓</span>
+    </div>
   </div>
 );
